45ed320ed3087573b28adb77ba63a5a73251a953
Andre Henriques <aphenriques@outlook.com>
2014-10-05 18:46:23 -0300

    Makefile corrections
    - fixed some pre-defined variable names;
    - fixed samples dependency to integral static library.

--- common.mk
+++ common.mk
@@ -5,8 +5,8 @@ INSTALL_LIB= $(INSTALL_TOP)/lib
 INTEGRAL_STATIC_LIB_NAME:=libintegral.a
 INTEGRAL_SHARED_LIB_NAME:=libintegral.so
 
-CPPFLAGS:=-std=c++11 -O3 -I/usr/local/include -I/usr/include/lua5.2
-#CPPFLAGS:=-std=c++11 -O3 -I/usr/local/include/luajit-2.0
+CXXFLAGS:=-std=c++11 -O3 -I/usr/local/include -I/usr/include/lua5.2
+#CXXFLAGS:=-std=c++11 -O3 -I/usr/local/include/luajit-2.0
 LDFLAGS:=-L/usr/local/lib -L/usr/lib/x86_64-linux-gnu/ -L/usr/lib
 
 LIB_LUA_FLAG:=-llua
@@ -18,5 +18,5 @@ UNAME_S:=$(shell uname -s)
 ifeq ($(UNAME_S),Darwin)
 SHARED_LIB_FLAGS+=-undefined dynamic_lookup
 else
-CPPFLAGS+=-fPIC
+CXXFLAGS+=-fPIC
 endif
