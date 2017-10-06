ab382048de70cb70a6b6bd5281c400574a8f2b5c
aphenriques <aphenriques@outlook.com>
2017-10-06 22:37:05 +0000

    added -Werror flag

--- common.mk
+++ common.mk
@@ -11,7 +11,7 @@ PROJECT_LDLIBS=$(PROJECT_ROOT_DIR)/$(PROJECT_LIB_DIR)/$(PROJECT_STATIC_LIB)
 PROJECT_SYSTEM_INCLUDE_DIRS:=/usr/local/include
 PROJECT_LIB_DIRS:=/usr/local/lib
 # TODO with c++17: remove -Wno-unused-parameter and -Wno-unused-but-set-parameter and use [[maybe_unused]] attribute
-PROJECT_CXXFLAGS:=-std=c++14 -O0 -Wall -Wextra -Wshadow -Wnon-virtual-dtor -Wno-missing-braces -Wno-unused-parameter -pedantic
+PROJECT_CXXFLAGS:=-std=c++14 -O0 -Werror -Wall -Wextra -Wshadow -Wnon-virtual-dtor -Wno-missing-braces -Wno-unused-parameter -pedantic
 
 ifeq ($(shell uname -s),Darwin)
 # -Wweak-vtables is a clang feature
