d8ab0667376a0db41ab7f8c02aa6d7ab9a2fddda
André Henriques <aphenriques@hotmail.com>
2017-12-18 22:23:53 -0200

    fixed makefile assignment type

--- common.mk
+++ common.mk
@@ -10,7 +10,7 @@ PROJECT_STATIC_LIB:=lib$(PROJECT).a
 # some variables require PROJECT_ROOT_DIR definition. That's why = is used instead of := for their definition
 PROJECT_INCLUDE_DIRS=$(PROJECT_ROOT_DIR)/$(PROJECT_LIB_ROOT_DIR)
 PROJECT_LDLIBS=$(PROJECT_ROOT_DIR)/$(PROJECT_LIB_DIR)/$(PROJECT_STATIC_LIB)
-PROJECT_SYSTEM_INCLUDE_DIRS:=/usr/local/include $(PROJECT_ROOT_DIR)/$(PROJECT_DEPENDENCIES_DIR)/exception/include
+PROJECT_SYSTEM_INCLUDE_DIRS=/usr/local/include $(PROJECT_ROOT_DIR)/$(PROJECT_DEPENDENCIES_DIR)/exception/include
 PROJECT_LIB_DIRS:=/usr/local/lib
 # '?=' sets the variable if it was not previously set
 OPTIMIZATION_FLAG?=-O0
